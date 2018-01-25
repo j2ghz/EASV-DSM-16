b40556f2a3b257cbc0c6070c549b2cc751c3f1ec
J2ghz <j2.00ghz@gmail.com>
2018-01-25 13:07:52 +0100

    redo

--- WEB/BikeRental/BikeRental/routes/bikes/index.ts
+++ WEB/BikeRental/BikeRental/routes/bikes/index.ts
@@ -1,12 +1,21 @@
 ﻿import * as express from "express";
 import { Request, Response } from "express";
+import {json} from "body-parser";
 var router = express.Router();
+var data: Array<Bike> = require("./data");
 router.get("/",
     (req: Request, res: Response) => {
-        res.status(200).json(require("./data").bikes);
+        res.status(200).json(data);
     });
 router.get("/:id",
     (req: Request, res: Response) => {
-        res.status(200).json(require("./data").bikes.find((m: Bike) => m.id === parseInt(req.param("id", 0))));
+        res.status(200).json(data.find((m: Bike) => m.id === parseInt(req.param("id", 0))));
+    });
+router.post("/",
+    json,
+    (req: Request, res: Response) => {
+        var toAdd: Bike = req.body;
+        data.push(toAdd);
+        res.status(204);
     });
 export = router;
