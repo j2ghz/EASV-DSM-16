86b1de0c20cd0be9c029b565e55b92c10c83af7f
J2ghz <j2.00ghz@gmail.com>
2018-01-30 11:29:36 +0100

    use mongo

--- WEB/BikeRental/BikeRental/server.ts
+++ WEB/BikeRental/BikeRental/server.ts
@@ -1,5 +1,11 @@
 ﻿import * as express from "express";
 import routes from "./routes";
+import { MongoClient } from "mongodb";
+
 let app = express();
 app.use("/", routes);
 app.listen(process.env.port || 1337);
+
+//MongoClient.connect("mongo:/localhost:27017/", (err, db) => {
+//    db.db("BikeRental").collection("bikes")
+//} );
