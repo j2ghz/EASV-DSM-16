e7afe0818127251c0eea382f0ecc5799506d6646
J2ghz <j2.00ghz@gmail.com>
2018-01-31 10:17:38 +0100

    Working step1 of catchthemouse

--- Xamarin/CatchTheMouse/CatchTheMouse/CatchTheMouse/MainPage.xaml.cs
+++ Xamarin/CatchTheMouse/CatchTheMouse/CatchTheMouse/MainPage.xaml.cs
@@ -1,17 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace CatchTheMouse
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        private readonly Random r = new Random();
+        private DateTime Start;
+        private int counter = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            if (Start == default(DateTime))
+            {
+                Start = DateTime.Now;
+            }
+            else
+            {
+                counter++;
+            }
+            if (counter >= 20)
+            {
+                await DisplayAlert("Done", (DateTime.Now - Start).ToString(), "Dismiss");
+            }
+            AbsoluteLayout.SetLayoutBounds(Button, new Rectangle(r.NextDouble(), r.NextDouble(), 0.01, 0.01));
+            
+        }
+    }
 }
---
+++ Xamarin/CatchTheMouse/README.md
@@ -0,0 +1,12 @@
+Lets use the touch functionality of your device.
+* Step 1:
+  * Create a display that can:
+  * Display a Button at an arbitrary position
+  * When clicked move the Button to another positon
+  * Measure the time it takes to click 20 Buttons
+* Step 2:
+  * Create a display in which you can add your name to your score and store it in a collection.
+  * Store the collection in the Application property dictionary (Yes it is not transient data!!) in the application onsleep
+  * Restore the collection again from the application onstart	
+* Step 3:
+  * Finalize the app with navigation (Carousel or Navigation page) and add some colours and boxviews etc.
