779e74c939af7f76e001c247218fa36d62fb00e3
J2ghz <j2.00ghz@gmail.com>
2018-02-01 10:43:34 +0100

    button stop blinking

---
+++ RaspberryPi/misc/buttonStopBlink.py
@@ -0,0 +1,25 @@
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+def on(n):
+    GPIO.output(n, True)
+def off(n):
+    GPIO.output(n, False)
+
+t=0.1
+
+while True:
+    on(26)
+    off(21)
+    sleep(t)
+    while not GPIO.input(18):
+        sleep(t)
+    off(26)
+    on(21)
+    sleep(t)
+    while not GPIO.input(18):
+        sleep(t)
