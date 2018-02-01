656740d01b8c54c11fadb0c2f52b5f414bf04aef
J2ghz <j2.00ghz@gmail.com>
2018-02-01 11:15:21 +0100

    blink faster while holding, 4 leds

--- RaspberryPi/misc/buttonStopBlink.py
+++ RaspberryPi/misc/buttonStopBlink.py
@@ -5,25 +5,33 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(26, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
 def on(n):
     GPIO.output(n, True)
 def off(n):
     GPIO.output(n, False)
 
-t=0.1
+
+def check():
+    t = 0
+    if not GPIO.input(18):
+        t = 0.08
+    else:
+        t = 0.15
+    sleep(t)
 
 while True:
-    if not GPIO.input(18):
-        t = 0.1
-    else:
-        t = 0.01
-    off(20)
+    
+    off(16)
     on(26)    
-    sleep(t)
+    check()
     off(26)
     on(21)
-    sleep(t)
+    check()
     off(21)
     on(20)
-    sleep(t)
+    check()
+    off(20)
+    on(16)
+    check()
