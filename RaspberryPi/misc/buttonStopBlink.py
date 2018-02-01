8b26d8ed00ed7515ceecda615f63fb9cf80395e8
J2ghz <j2.00ghz@gmail.com>
2018-02-01 11:05:02 +0100

    blink faster while holding

--- RaspberryPi/misc/buttonStopBlink.py
+++ RaspberryPi/misc/buttonStopBlink.py
@@ -4,22 +4,24 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(26, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
 
 def on(n):
     GPIO.output(n, True)
 def off(n):
     GPIO.output(n, False)
 
-t=0.05
+t=0.1
 
 while True:
-    off(21)
+    off(20)
     on(26)    
     sleep(t)
-    while not GPIO.input(18):
-        sleep(t)
     off(26)
     on(21)
     sleep(t)
-    while not GPIO.input(18):
-        sleep(t)
+    off(21)
+    on(20)
+    sleep(t)
+    if not GPIO.input(18):
+        t = 0.9 * t
