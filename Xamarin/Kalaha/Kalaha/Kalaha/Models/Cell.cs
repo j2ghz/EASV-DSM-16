0207f922224912bbe10106ff65f466c34c674142
J2ghz <j2.00ghz@gmail.com>
2018-02-04 20:04:32 +0100

    ideas

--- Xamarin/Kalaha/Kalaha/Kalaha/Models/Cell.cs
+++ Xamarin/Kalaha/Kalaha/Kalaha/Models/Cell.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactiveUI;
 
 namespace Kalaha.Models
 {
-    public class Cell
+    public class Cell : ReactiveObject
     {
+        public Cell Next { get; set; }
+        public byte Seeds { get; set; }
+
+        public void MoveSeeds()
+        {
+        }
+
+        public void MoveSeeds(byte seedCount)
+        {
+            Seeds++;
+            if (seedCount > 0)
+                Next.MoveSeeds(--seedCount);
+            //if end at own house, one more move
+            //maybe different types for house/store?
+
+        }
     }
 }
