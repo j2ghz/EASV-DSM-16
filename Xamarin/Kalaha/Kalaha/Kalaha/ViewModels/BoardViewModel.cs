bbfef81fb38acbfe15d9185e25e843b9e7b54580
J2ghz <j2.00ghz@gmail.com>
2018-02-04 17:15:29 +0100

    kalaha

---
+++ Xamarin/Kalaha/Kalaha/Kalaha/ViewModels/BoardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kalaha.Models;
+
+namespace Kalaha.ViewModels
+{
+    public class BoardViewModel : ViewModelBase
+    {
+        public Board Board { get; }
+
+        public BoardViewModel(Board board)
+        {
+            this.Board = board;
+        }
+    }
+}
---
+++ Xamarin/Kalaha/Kalaha/Kalaha/Views/BoardView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kalaha.ViewModels;
+using ReactiveUI.XamForms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kalaha.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BoardView : ReactiveContentView<BoardViewModel>
+	{
+		public BoardView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
