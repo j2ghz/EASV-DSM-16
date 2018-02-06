d0a66215d457529324095b1ce442c72ccfecb274
J2ghz <j2.00ghz@gmail.com>
2018-02-06 10:22:48 +0100

    WTF FIX, now it works

--- WEB/BikeRental/BikeRental/database.ts
+++ WEB/BikeRental/BikeRental/database.ts
@@ -1,6 +1,5 @@
-﻿import * as mongoose from "mongoose";
+﻿import mongoose = require("mongoose") //TODO FIX
 
 (mongoose as any).Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/BikeRental");
-
 export { mongoose };
