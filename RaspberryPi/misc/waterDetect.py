4841c674fc0a846473281f22d4add59ffa13ca52
J2ghz <j2.00ghz@gmail.com>
2018-02-06 09:36:58 +0100

    finish water

--- RaspberryPi/misc/waterDetect.py
+++ RaspberryPi/misc/waterDetect.py
@@ -4,20 +4,35 @@ GPIO.setmode(GPIO.BCM)
 LOW=4
 MID=5
 HIGH=13
+LLOW=16
+LMID=20
+LHIGH=21
 
 GPIO.setup(LOW, GPIO.IN)
 GPIO.setup(MID, GPIO.IN)
 GPIO.setup(HIGH, GPIO.IN)
+GPIO.setup(LLOW, GPIO.OUT)
+GPIO.setup(LMID, GPIO.OUT)
+GPIO.setup(LHIGH, GPIO.OUT)
 
-log = open("log.txt","a")
-log.wri
+def on(n):
+    GPIO.output(n, True)
+    print(n)
+def off(n):
+    GPIO.output(n, False)
 
 while True:
     print(asctime())
     if GPIO.input(HIGH) == 1:
-        print("High")
+        on(LHIGH)
+    else:
+        off(LHIGH)
     if GPIO.input(MID) == 1:
-        print("Mid")
+        on(LMID)
+    else:
+        off(LMID)
     if GPIO.input(LOW) == 1:
-        print("Low")
+        on(LLOW)
+    else:
+        off(LLOW)
     sleep(1)
