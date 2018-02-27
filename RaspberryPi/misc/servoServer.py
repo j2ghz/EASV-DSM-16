743f562dac5ac6f2e94f9e1b0a2dec8fe0103b74
J2ghz <j2.00ghz@gmail.com>
2018-02-27 09:36:46 +0100

    servoserver

--- RaspberryPi/misc/servoServer.py
+++ RaspberryPi/misc/servoServer.py
@@ -11,17 +11,31 @@ p = GPIO.PWM(26, 50)
 p.start(7.5)
 
 TCP_IP = '0.0.0.0'
-TCP_PORT = 9999
+TCP_PORT = 9997
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create a server using IPv4 and TCP
 s.bind((TCP_IP, TCP_PORT)) # listen on this IP and PORT
 s.listen(1) # start listening
+while True:
+    # Wait for a connection
+    print 'waiting for a connection'
+    connection, client_address = s.accept()
+    try:
+        print 'connection from', client_address
 
-conn, addr = s.accept() # acceppt a connection
-print 'Client connected from:', addr
-while 1:
-    data = conn.recv(1024) # receive data
-    if not data: break #if no more data, stop loop
-    print "received data:", data #print received data
-    conn.send(data)  # send the same message back to client
-conn.close() # close the connection
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(16)
+            print 'received "%s"' % data
+            if data:
+                if data=="l":
+                    p.ChangeDutyCycle(2.5)
+                if data=="r":
+                    p.ChangeDutyCycle(12.5)
+            else:
+                print 'no more data from', client_address
+                break
+            
+    finally:
+        # Clean up the connection
+        connection.close()
