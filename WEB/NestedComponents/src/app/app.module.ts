f0354beec049693b94657a2e754d5f757081f584
J2ghz <j2.00ghz@gmail.com>
2018-02-28 11:26:08 +0100

    Add the nested components

--- WEB/NestedComponents/src/app/app.module.ts
+++ WEB/NestedComponents/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BikesComponent } from './bikes/bikes.component';
+import { BikeListComponent } from './bikes/bike-list/bike-list.component';
+import { BikeRowComponent } from './bikes/bike-list/bike-row/bike-row.component';
+import { BikeComponent } from './bikes/bike-list/bike-row/bike/bike.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikesComponent,
+    BikeListComponent,
+    BikeRowComponent,
+    BikeComponent
   ],
   imports: [
     BrowserModule
