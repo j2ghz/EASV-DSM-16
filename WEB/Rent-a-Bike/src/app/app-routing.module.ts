3f62feda24908c24875e1304d2dac4917c44a312
J2ghz <j2.00ghz@gmail.com>
2018-04-12 22:08:47 +0200

    dashboard links

--- WEB/Rent-a-Bike/src/app/app-routing.module.ts
+++ WEB/Rent-a-Bike/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BikesComponent } from './bikes/bikes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { BikeDetailComponent }  from './bikes/bike-detail/bike-detail.component';
 
 const routes: Routes = [
-  { path: 'bikes', component: BikesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'bikes', component: BikesComponent },
+  { path: 'detail/:id', component: BikeDetailComponent }
 ];
 
 @NgModule({
--- WEB/Rent-a-Bike/src/app/dashboard/dashboard.component.ts
+++ WEB/Rent-a-Bike/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Bike } from '../bike';
+import { BikeService } from '../bikes/bike.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
+  bikes: Bike[] = [];
 
-  constructor() { }
+  constructor(private bikeService: BikeService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.bikeService.getBikes()
+      .subscribe(bikes => this.bikes = bikes.slice(1, 5));
+  }
 }
