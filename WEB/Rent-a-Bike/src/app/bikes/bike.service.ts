e32d16f2269f9fa911697e25d3077a9e744b115b
J2ghz <j2.00ghz@gmail.com>
2018-04-13 21:03:30 +0200

    mock http

--- WEB/Rent-a-Bike/src/app/bikes/bike.service.ts
+++ WEB/Rent-a-Bike/src/app/bikes/bike.service.ts
@@ -1,23 +1,55 @@
 import { Injectable } from '@angular/core';
 import { Bike } from '../bike';
-import { BIKES } from '../mock-bikes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from '../messages/message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class BikeService {
+  private bikesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getBikes(): Observable<Bike[]> {
-    this.messageService.add('BikeService: fetched bikes at ' + new Date().toLocaleString());
-    return of(BIKES);
+    return this.http.get<Bike[]>(this.bikesUrl)
+      .pipe(
+        tap(bikes => this.log('fetched bikes')),
+        catchError(this.handleError('getHeroes', []))
+      );
   }
 
   getBike(id: number): Observable<Bike> {
-    this.messageService.add(`BikeService: fetched bike with id=${id} at ` + new Date().toLocaleString());
-    return of(BIKES.find(bike=> bike.id === id));
+    const url = `${this.bikesUrl}/${id}`;
+    return this.http.get<Bike>(url).pipe(
+      tap(_ => this.log(`fetched bike id=${id}`)),
+      catchError(this.handleError<Bike>(`getBike id=${id}`))
+    );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    this.messageService.add('BikeService: ' + message);
   }
 
 }
