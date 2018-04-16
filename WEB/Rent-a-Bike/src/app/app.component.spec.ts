f29b5bc5b0ca9b9beecd9479b75be4d16e033a96
J2ghz <j2.00ghz@gmail.com>
2018-04-16 13:44:09 +0200

    fix tests

--- WEB/Rent-a-Bike/src/app/app.component.spec.ts
+++ WEB/Rent-a-Bike/src/app/app.component.spec.ts
@@ -1,13 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+import { Component } from '@angular/core';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
+
+    @Component({selector: 'app-messages', template: ''})
+    class MessagesStubComponent {}
+
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MessagesStubComponent
       ],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -22,6 +35,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('app');
   }));
 });
