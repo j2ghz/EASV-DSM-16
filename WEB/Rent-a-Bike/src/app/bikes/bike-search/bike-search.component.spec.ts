6c63f9985d51efb4d8262e8d96f28df09785f179
J2ghz <j2.00ghz@gmail.com>
2018-04-18 17:04:47 +0200

    fix search tests

--- WEB/Rent-a-Bike/src/app/bikes/bike-search/bike-search.component.spec.ts
+++ WEB/Rent-a-Bike/src/app/bikes/bike-search/bike-search.component.spec.ts
@@ -1,16 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BikeSearchComponent } from './bike-search.component';
+import { RouterLinkDirectiveStub } from '../../../testing/router-link-directive-stub';
+import { BikeService } from '../bike.service';
+import { Bike } from '../../bike';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 describe('BikeSearchComponent', () => {
   let component: BikeSearchComponent;
   let fixture: ComponentFixture<BikeSearchComponent>;
+  let bikeServiceStub: Partial<BikeService>;
 
   beforeEach(async(() => {
+    bikeServiceStub = {
+      searchBikes: (term: string) => {
+        return of([{ id: 0, name: "Test" }])
+      }
+    }
+
     TestBed.configureTestingModule({
-      declarations: [ BikeSearchComponent ]
+      declarations: [
+        BikeSearchComponent,
+        RouterLinkDirectiveStub
+      ],
+      providers: [
+        { provide: BikeService, useValue: bikeServiceStub }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
